fd2184ecc1eb12943e135a0a8fe4b06e6edb609d
eaverdeja <eaverdeja@gmail.com>
2025-01-22 15:01:23 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,27 +8,41 @@ import sys
 #
 # - decode_bencode(b"5:hello") -> b"hello", 7
 # - decode_bencode(b"10:hello12345") -> b"hello12345", 13
-def decode_bencode(s: bytes, pos=0) -> tuple[bytes, int]:
-    char_at_pos = chr(s[pos])
-    if char_at_pos == "l":
+def decode_bencode(bencode: bytes, pos=0) -> tuple[bytes, int]:
+    char_at_pos = chr(bencode[pos])
+    if char_at_pos == "l":  # lists
         items = []
         pos += 1  # +1 to account for the start of list marker
-        while pos < len(s):
-            if chr(s[pos]) == "e":
+        while pos < len(bencode):
+            if chr(bencode[pos]) == "e":
                 break
-            item, pos = decode_bencode(s, pos)
+            item, pos = decode_bencode(bencode, pos)
             items.append(item)
 
         return items, pos + 1  # +1 to account for the end of list marker
-    elif char_at_pos == "i":
-        end = s.index(b"e", pos)
-        return int(s[pos + 1 : end]), end + 1  # +1 to account for the end of int marker
-    elif char_at_pos.isdigit():
-        colon = s.index(b":", pos)
-        length = int(s[pos:colon])
+    elif char_at_pos == "d":  # dictionaries
+        items = {}
+        pos += 1
+        while pos < len(bencode):
+            if chr(bencode[pos]) == "e":
+                break
+            key, pos = decode_bencode(bencode, pos)
+            value, pos = decode_bencode(bencode, pos)
+            items[key.decode()] = value
+
+        return items, pos + 1  # +1 to account for the end of dictionary marker
+    elif char_at_pos == "i":  # integers
+        end = bencode.index(b"e", pos)
+        return (
+            int(bencode[pos + 1 : end]),
+            end + 1,
+        )  # +1 to account for the end of int marker
+    elif char_at_pos.isdigit():  # strings
+        colon = bencode.index(b":", pos)
+        length = int(bencode[pos:colon])
         start = colon + 1
         end = start + length
-        return s[start:end], end
+        return bencode[start:end], end
     else:
         raise NotImplementedError(
             f"Only strings are supported at the moment - got: {char_at_pos}"
